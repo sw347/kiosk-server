4014b835e8a04cf64419e64aa2d5584c9c2419c6
sw347 <sdh230310@sdh.hs.kr>
2025-10-02 10:00:17 +0200

    chore: cors 설정

--- config/settings.py
+++ config/settings.py
@@ -16,8 +16,13 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000"
+# ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -29,10 +34,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'core',
     'products',
-    'orders'
+    'orders',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
